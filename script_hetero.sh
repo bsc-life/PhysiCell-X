69ee4d7b146f6d2faf518f16974e6f2b5667ce84
Gaurav Saxena <gauravsaxena@hotmail.com>
2020-02-07 11:14:27 +0100

    (1) Initial Commit, see README.md for the initial preparation that must be made. (2) Had to remove DistPhy from list of commited files as it complaints that it is a sub git project. Removed from staged files using $git rm -f --cached DistPhy (3) Will be maintaining DistPhy as a separate project repository.

---
+++ script_hetero.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=2
+#SBATCH -t 00:60:00
+#SBATCH -o output-%j
+#SBATCH -e error-%j
+#SBATCH --exclusive
+
+#export OMP_SCHEDULE=STATIC
+#export OMP_DISPLAY_ENV=true
+#export OMP_NUM_THREADS=48
+#export OMP_PROC_BIND=spread
+#export OMP_PLACES="{0:1}:48:1"
+#export OMP_PLACES='cores(48)'
+#mpiexec --map-by ppr:1:socket:pe=24  --report-bindings ./examples/tutorial1
+mpiexec ./heterogeneity.exe
+#mpiexec --map-by socket --bind-to core  --report-bindings ./examples/tutorial1
+#mpiexec --map-by node --bind-to none --report-bindings ./examples/tutorial1
